9f54210839936192ab32dc87f7fa9f878e43de1a
jcruz <jcruz@jcruz>
2016-09-05 21:19:55 -0300

    ajuste no archetype

---
+++ sk-project/src/main/resources/archetype-resources/src/main/java/Application.java
@@ -0,0 +1,23 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import java.io.IOException;
+
+import sk.api.SkApplication;
+import sk.api.annotation.ApplicationConf;
+
+/**
+ * Hello world!
+ *
+ */
+@ApplicationConf(uuid = "${artifactId}", description = "${sk-app-description}")
+public class Application implements SkApplication {
+
+	@Override
+	public void run() throws IOException {
+		System.out.println("It's works!");
+	}
+
+}
